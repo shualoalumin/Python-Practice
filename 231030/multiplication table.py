456e8b45f05b64eeeb5f47f01128f4ea3e258e46
Josh Kim <realx1212@gmail.com>
2024-10-31 01:18:54 +0900

    Update multiplication table.py
    added a blank line b/t each table

--- 231030/multiplication table.py
+++ 231030/multiplication table.py
@@ -2,8 +2,7 @@
 for dan in range(2, 10):
     for num in range(1, 10):
         print(f"{dan} x {num} = {dan * num}")
-        if num == 10
-        print()
+    print()
 
 #원하는 구구단 입력하기
 n = int(input("원하는 단수를 입력하세요:"))
