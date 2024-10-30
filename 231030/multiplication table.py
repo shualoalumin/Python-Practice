14ec035e22f8532a2519be177c31c9c584615e42
Josh Kim <realx1212@gmail.com>
2024-10-31 00:22:15 +0900

    Create multiplication table.py

---
+++ 231030/multiplication table.py
@@ -0,0 +1,11 @@
+#구구단 전체
+for dan in range(2, 10):
+    for num in range(1, 10):
+        print(f"{dan} x {num} = {dan * num}")
+        if num == 10
+        print()
+
+#원하는 구구단 입력하기
+n = int(input("원하는 단수를 입력하세요:"))
+for x in range(1, 10):
+    print(f"{n} x {x} = {n * x}")
