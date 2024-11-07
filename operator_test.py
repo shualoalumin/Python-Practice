1c7863f74dc5058a4f378af531a67e2010fdd354
Josh Kim <realx1212@gmail.com>
2024-11-07 14:54:44 +0900

    Create operator_test.py

---
+++ operator_test.py
@@ -0,0 +1,17 @@
+
+
+
+
+
+
+# 연산자함수 테스트
+
+import math
+
+def operator_input():
+        operator = input("연산자를 입력하세요(+,-,*,/,**): ")
+        if operator in ['+','-','*','/','**']:
+            return operator
+
+        else:
+            print("지원하지 않는 연산자입니다.")
