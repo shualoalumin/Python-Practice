ea5bd7a06d86d94229827f995fd254c6b74de76f
Josh Kim <realx1212@gmail.com>
2024-11-19 23:44:09 +0900

    Update 프로그래머스.py

--- 프로그래머스.py
+++ 프로그래머스.py
@@ -1,15 +1,15 @@
+def solution(n):
+    if n % 2 == 1:  # n이 홀수인 경우
+        answer = 0  # 홀수의 합을 저장할 변수 초기화
+        for num in range(1, n + 1, 2):  # 홀수만 반복
+            answer += num  # 홀수를 합계에 추가
+        return answer  # 최종 홀수의 합 반환
+    else:  # n이 짝수인 경우
+        answer = 0  # 짝수의 제곱의 합을 저장할 변수 초기화
+        for num in range(2, n + 1, 2):  # 짝수만 반복
+            answer += num ** 2  # 짝수의 제곱을 합계에 추가
+        return answer  # 최종 짝수의 제곱의 합 반환
+    
+n = int(input("양의정수:"))
+print(solution(n))
 
-2
-3
-4
-5
-6
-7
-def solution(my_string):
-    answer = []
-    for i in range(len(my_string)):
-        answer.append(my_string[-i:])
-    answer.sort()
-    return answer
-
-print(solution("banana"))
